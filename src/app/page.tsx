176336a693df866e19b28c0004246e257ecfde5d
jasisamuran <samuran3132@gmail.com>
2025-06-03 07:01:02 +0530

    socket implementation to check working correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,43 @@
-export default function Home() {
+"use client";
+import { useEffect, useState } from "react";
+import socket from "../lib/socket";
+function Home() {
+  const [message, setMessage] = useState("");
+  const [chat, setChat] = useState<string[]>([]);
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setChat((prev) => [...prev, data]);
+    });
+
+    return () => {
+      socket.off("receive_message");
+    };
+  }, []);
+
+  const sendMessage = () => {
+    socket.emit("send_message", message);
+    setMessage("");
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Welcom to Notification Dashboard
+    <div style={{ padding: 20 }}>
+      <h2>Socket.IO Chat</h2>
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Type a message"
+      />
+      <button onClick={sendMessage}>Send</button>
+
+      <ul>
+        {chat.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export default Home;
